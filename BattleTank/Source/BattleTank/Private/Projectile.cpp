1c02dbf30a7e51fd2f31b04600e29d628471ee13
Gregory Scott Hanna <eagle_knight_22@hotmail.com>
2018-09-14 18:01:24 -0400

    Issue with Projectile Particle System Details tab being blank. Partially through lecture 198

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -21,7 +21,7 @@ AProjectile::AProjectile()
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
 	SetRootComponent(CollisionMesh);
 	CollisionMesh->SetNotifyRigidBodyCollision(true);
-	CollisionMesh->SetVisibility(false);
+	///CollisionMesh->SetVisibility(false);
 
 	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
 	LaunchBlast->SetupAttachment(RootComponent);
